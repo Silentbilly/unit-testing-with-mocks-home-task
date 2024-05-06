deb7f682c6f2d94f51e364cb70981135d2f6feab
Alexander_Kononov1 <alexander_kononov1@epam.com>
2024-05-06 16:16:15 +0300

    finished hometask

---
+++ test/nock_test.js
@@ -0,0 +1,116 @@
+const path = require('path')
+const sinon = require('sinon')
+const fs = require('fs')
+const assert = require('assert')
+const UserDataHandler = require('../src/data_handlers/user_data_handler')
+const nock = require('nock')
+
+describe('Test user data handler with nock', function () {
+  let users
+  let userDataHandler
+  let userEmailList
+  let usersStub
+  const pathToUsersJson = path.join(__dirname, '..', 'data', 'users.json')
+  const responseData = JSON.parse(fs.readFileSync(pathToUsersJson))
+  userDataHandler = new UserDataHandler()
+  const isMatchingAllSearchParamsStub = sinon.stub(userDataHandler, 'isMatchingAllSearchParams')
+  usersStub = sinon.stub(userDataHandler, 'users')
+  const findUsersStub = sinon.stub(userDataHandler, 'findUsers')
+
+  beforeEach(async () => {
+    nock('http://localhost:3000').get('/users')
+      .reply(200, responseData)
+
+    await userDataHandler.loadUsers()
+    users = userDataHandler.users
+    userEmailList = userDataHandler.getUserEmailsList()
+    numberOfUsers = userDataHandler.getNumberOfUsers()
+  })
+
+  it('Uers list test', async function () {
+    assert.deepEqual(users, responseData)
+  })
+
+  it('Handle error when loading users fails', async () => {
+    const errorMessage = 'Failed to load users data: Network Error'
+    nock('http://localhost:3000').get('/users').replyWithError({
+      message: 'Failed to load users data'
+    })
+
+    assert.rejects(userDataHandler.loadUsers(), errorMessage)
+  })
+
+  it('Email list test', async function () {
+    const expectedEmailList = responseData.map(user => user.email).join(';')
+
+    assert.equal(userEmailList, expectedEmailList)
+  })
+
+  it('Number of users test', async function () {
+    const expectedNumberOfUsers = responseData.length
+    const numberOfUsers = userDataHandler.getNumberOfUsers()
+
+    assert.equal(numberOfUsers, expectedNumberOfUsers)
+  })
+
+  it('isMatchingAllSearchParams test', async function () {
+    isMatchingAllSearchParamsStub.withArgs(sinon.match.any, sinon.match.any)
+      .returns(true)
+    assert.equal(userDataHandler.isMatchingAllSearchParams({ /* user object */ }, { /* searchParamsObject */ }), true)
+  })
+
+  it('Handle error when finding users fails', async () => {
+    const errorMessage = 'No search parameters provoded!'
+    isMatchingAllSearchParamsStub.withArgs(sinon.match.any, sinon.match.any).rejects(new Error(errorMessage))
+
+    assert.rejects(userDataHandler.findUsers(sinon.match.any), errorMessage)
+
+    isMatchingAllSearchParamsStub.withArgs(sinon.match.any, sinon.match.any).returns(true)
+  })
+
+  it('Finding matching users', async () => {
+    findUsersStub.withArgs(sinon.match.any).returns(users)
+
+    assert.deepEqual(
+      userDataHandler.findUsers({}),
+      responseData
+    )
+    findUsersStub.restore()
+  })
+
+  it('Handle error when no search parameters provided', async () => {
+    const errorMessage = 'No search parameters provoded!'
+
+    assert.throws(
+      () => userDataHandler.findUsers(!{}),
+      Error(errorMessage)
+    )
+    findUsersStub.restore()
+  })
+
+  it('Handle error when no users loaded', async () => {
+    const errorMessage = 'No users loaded!'
+    usersStub.value([])
+
+    assert.throws(
+      () => userDataHandler.findUsers({}),
+      Error(errorMessage)
+    )
+
+    findUsersStub.restore()
+    usersStub.restore()
+  })
+
+  it('Handle error when no matching users found', async () => {
+    const errorMessage = 'No matching users found!'
+    isMatchingAllSearchParamsStub.withArgs(sinon.match.any, sinon.match.any)
+      .returns(false)
+
+    assert.throws(
+      () => userDataHandler.findUsers({}),
+      Error(errorMessage)
+    )
+    findUsersStub.restore()
+    isMatchingAllSearchParamsStub.restore()
+  })
+})
